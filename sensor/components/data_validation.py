a32dba01857d1de550d4810b6b7445b52dfeb429
Hp <mutalikdesai.akshay412@gmail.com>
2022-11-19 18:36:38 +0530

    done

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -1,10 +1,13 @@
 from sensor.constants.training_pipeline import SCHEMA_FILE_PATH
+from distutils import dir_util
 from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from sensor.entity.config_entity import DataValidationConfig
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.utils.main_utils import read_yaml_file
-from pandas import DataFrame
+from scipy.stats import ks_2samp
+import numpy as np
+import pandas as pd
 import os,sys
 
 class DataValidation:
@@ -25,7 +28,9 @@ class DataValidation:
             if len(dataframe.columns)==number_of_columns:
                 return True
             return False
-        pass
+        except Exception as e:
+            raise SensorException(e,sys)
+
 
     def is_numeric_column_exist(self,dataframe:pd.DataFrame)->bool:
         try:
@@ -51,10 +56,46 @@ class DataValidation:
     @staticmethod
     def read_data(file_path)->pd.DataFrame:
         try:
-            pd.read_csv(file_path)
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise SensorException(e,sys)
 
-    def detect_dataset_drift(self):
-        pass
+    def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        """
+        Checks whether d1 and d2 have same distributions using hypothesis testing
+        if p value > 0.02 then we accept null hypothesis that the distributions d1
+        and d2 are same
+        """
+        try:
+            status = True
+            report = {}
+            for column in base_df:
+                d1 = base_df[column]
+                d2 = current_df[column]
+                is_same_dist = ks_2samp(d1,d2)
+                if threshold<=is_same_dist.pvalue:
+                    is_found = False
+                else:
+                    is_found = True
+                    status = False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status": is_found
+                }})
+
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content = report)
+            return status
+        except Exception as e:
+            raise SensorException (e,sys)
+
+
+
+
 
 
     def initiate_data_validation(self)->DataValidationArtifact:
@@ -85,8 +126,18 @@ class DataValidation:
             if len(error_message)>0:
                 raise Exception(error_message)
 
+            #Check for Data Drift
 
+            self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
 
+            data_validation_artifact = DataValidationArtifact(
+                validation_status= status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=self.data_validation_config.invalid_train_file_path,
+                invalid_test_file_path=self.data_validation_config.invalid_test_file_path,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path
+            )
 
         except Exception as e:
             raise SensorException(e,sys)
